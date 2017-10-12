655ece2265413f236dd2d3f09f9a8995f0cca6a9
Mikhail Gordeev <gordmisha@gmail.com>
2017-10-12 23:23:34 +0000

    Force update dotfiles

--- mkhomeconf.pl
+++ mkhomeconf.pl
@@ -10,8 +10,8 @@ print "@{[map {basename $_} @files[2..$#files]]}\n";
 
 foreach (@files) {
     my $file = basename $_;
-    print "ln -s $_ ~/$file \n" if $file !~ /\.?\.$/;
-    system "ln -s $_ ~/$file \n" if $file !~ /\.?\.$/;    
+    print "ln -fs $_ ~/$file \n" if $file !~ /\.?\.$/;
+    system "ln -fs $_ ~/$file \n" if $file !~ /\.?\.$/;
 }
 
 exit 0;
